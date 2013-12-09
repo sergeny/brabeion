82940b8fe2e5c12a6363cec625a9831aaf0c5cdb
Sergey Orshanskiy <orshansk@gmail.com>
2013-12-09 18:03:28 -0500

    Added support for custom BadgeAward models. Extend BadgeAwardAbstractClass, derive your (still abstract model), and set ELDARION_BASE_BADGE_AWARD_MODEL to your class in the settings. The final model will still be called BadgeAward.

--- brabeion/models.py
+++ brabeion/models.py
@@ -1,15 +1,14 @@
-from datetime import datetime
-
-#from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
-User = get_user_model()
-from django.db import models
 from django.utils import timezone
 
+from django.db import models
+
+from django.db import settings
+from django.utils.importlib import import_module
+import warnings
 
 
-class BadgeAward(models.Model):
-    user = models.ForeignKey(User, related_name="badges_earned")
+class BadgeAwardAbstractClass(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="badges_earned")
     awarded_at = models.DateTimeField(default=timezone.now)
     slug = models.CharField(max_length=255)
     level = models.IntegerField()
@@ -37,3 +36,33 @@ class BadgeAward(models.Model):
     @property
     def progress(self):
         return self._badge.progress(self.user, self.level)
+
+    class Meta:
+        abstract = True
+
+
+def get_base_model():
+    """
+    Determine the base Model to inherit in the
+    BadgeAward Model; this allows to overload it.
+    """
+    BASE_MODEL = getattr(settings, 'ELDARION_BASE_BADGE_AWARD_MODEL', '')
+    if not BASE_MODEL:
+        return BadgeAwardAbstractClass
+    dot = BASE_MODEL.rindex('.')
+    module_name = BASE_MODEL[:dot]
+    class_name = BASE_MODEL[dot + 1:]
+    try:
+        _class = getattr(import_module(module_name), class_name)
+        return _class
+    except (ImportError, AttributeError):
+        warnings.warn('%s cannot be imported' % BASE_MODEL,
+                      RuntimeWarning)
+    return BadgeAwardAbstractClass
+
+
+class BadgeAward(get_base_model()):
+    """
+    The final BadgeAward model based on inheritance.
+    This pattern was copied after django-blog-zinnia's overloadable Entry model.
+    """
